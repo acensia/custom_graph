025d0c1f95fca9505be7eb0e7cc08d8a6bd91e94
acensia <acensia@snu.ac.kr>
2024-07-10 23:55:28 +0900

    build init

--- custom_graph/index/__main__.py
+++ custom_graph/index/__main__.py
@@ -3,8 +3,15 @@ from .cli import index_cli
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    
+    parser.add_argument(
+        "--init",
+        help="",
+        action="store_true"
+    )
+
+
+    args = parser.parse_args()
 
     index_cli(
-        
+        init=args.init or False
     )
